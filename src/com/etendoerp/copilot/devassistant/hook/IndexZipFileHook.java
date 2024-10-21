aaf36c30d5d0e9a4111fe52e559e0023154ba71a
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-10-21 15:00:16 -0300

    Feature ETP-531: Improve AD_message and format Java

--- src/com/etendoerp/copilot/devassistant/hook/IndexZipFileHook.java
+++ src/com/etendoerp/copilot/devassistant/hook/IndexZipFileHook.java
@@ -51,186 +51,202 @@ public class IndexZipFileHook implements CopilotFileHook {
   protected static final String[] IGNORE_STRINGS = { ".git", "node_modules", ".idea", "/.", "/venv/", "/.venv/" };
   private static final Logger log = LogManager.getLogger(IndexZipFileHook.class);
 
- /**
- * Creates a ZIP file containing the specified set of files.
- * This method processes an array of search paths, identifies whether each path contains wildcards,
- * and handles each path accordingly to add matching files to the ZIP file.
- *
- * @param searchPaths The array of search paths to be processed.
- * @return The created ZIP file containing the files from the specified search paths.
- * @throws IOException If an I/O error occurs during the creation of the ZIP file.
- */
-public static File getCodeIndexZipFile(String[] searchPaths) throws IOException {
-  Set<Path> filesToZip = new HashSet<>();
+  /**
+   * Creates a ZIP file containing the specified set of files.
+   * This method processes an array of search paths, identifies whether each path contains wildcards,
+   * and handles each path accordingly to add matching files to the ZIP file.
+   *
+   * @param searchPaths
+   *     The array of search paths to be processed.
+   * @return The created ZIP file containing the files from the specified search paths.
+   * @throws IOException
+   *     If an I/O error occurs during the creation of the ZIP file.
+   */
+  public static File getCodeIndexZipFile(String[] searchPaths) throws IOException {
+    Set<Path> filesToZip = new HashSet<>();
 
-  for (String searchPath : searchPaths) {
-    // Trim whitespace
-    searchPath = searchPath.trim();
-    boolean hasWildcards = searchPath.contains("*") || searchPath.contains("?");
-    if (hasWildcards) {
-      handleWildcardPath(searchPath, filesToZip);
+    for (String searchPath : searchPaths) {
+      // Trim whitespace
+      searchPath = StringUtils.trim(searchPath);
+      boolean hasWildcards = StringUtils.contains(searchPath, "*") || StringUtils.contains(searchPath, "?");
+      if (hasWildcards) {
+        handleWildcardPath(searchPath, filesToZip);
+      } else {
+        handleSpecificFilePath(searchPath, filesToZip);
+      }
+    }
+    return getZipFile(filesToZip);
+  }
+
+  /**
+   * Handles specific files or directories without wildcards and adds them to the provided set.
+   * This method processes the given search path, normalizes it, and checks if it exists.
+   * If the path is a regular file and not ignored, it is added to the set.
+   * If the path is a directory, all files in the directory are added recursively.
+   *
+   * @param searchPath
+   *     The specific file or directory path to be processed.
+   * @param filesToZip
+   *     The set to which the files will be added.
+   * @throws IOException
+   *     If an I/O error occurs during file processing.
+   * @throws OBException
+   *     If the path does not exist or is invalid.
+   */
+  private static void handleSpecificFilePath(String searchPath, Set<Path> filesToZip) throws IOException {
+    // Handle specific files or directories without wildcards
+    Path path = Paths.get(searchPath).normalize();
+
+    if (!Files.exists(path)) {
+      throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_PATHEXISTENCEERROR"), path.toString()));
+    }
+
+    if (Files.isRegularFile(path)) {
+      if (checkIgnoredFiles(path.toString())) {
+        filesToZip.add(path);
+      }
+    } else if (Files.isDirectory(path)) {
+      // Add all files in directory recursively
+      Files.walkFileTree(path, getSimpleFileVisitor(filesToZip));
     } else {
-      handleSpecificFilePath(searchPath, filesToZip);
+      throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_INVALIDPATH"), path.toString()));
     }
   }
-  return getZipFile(filesToZip);
-}
 
- /**
- * Handles specific files or directories without wildcards and adds them to the provided set.
- * This method processes the given search path, normalizes it, and checks if it exists.
- * If the path is a regular file and not ignored, it is added to the set.
- * If the path is a directory, all files in the directory are added recursively.
- *
- * @param searchPath The specific file or directory path to be processed.
- * @param filesToZip The set to which the files will be added.
- * @throws IOException If an I/O error occurs during file processing.
- * @throws OBException If the path does not exist or is invalid.
- */
-private static void handleSpecificFilePath(String searchPath, Set<Path> filesToZip) throws IOException {
-  // Handle specific files or directories without wildcards
-  Path path = Paths.get(searchPath).normalize();
+  /**
+   * Handles paths with wildcards and adds matching files to the provided set.
+   * This method processes the given search path, identifies the base path and glob pattern,
+   * and walks the file tree from the base path to find matching files.
+   *
+   * @param searchPath
+   *     The search path containing wildcards.
+   * @param filesToZip
+   *     The set to which matching files will be added.
+   * @throws IOException
+   *     If an I/O error occurs during file tree traversal.
+   */
+  private static void handleWildcardPath(String searchPath, Set<Path> filesToZip) throws IOException {
+    String globPattern;
+    Path basePath;
+    // Handle paths with wildcards
+    int firstWildcardIndex = indexOfWildcard(searchPath);
+    int lastSeparatorBeforeWildcard = searchPath.lastIndexOf(File.separator, firstWildcardIndex);
 
-  if (!Files.exists(path)) {
-    throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_PATHEXISTENCEERROR"), path.toString()));
-  }
+    // Base path is up to the last separator before the first wildcard
+    String basePathString = (lastSeparatorBeforeWildcard >= 0)
+        ? StringUtils.substring(searchPath, 0, lastSeparatorBeforeWildcard)
+        : ".";
+    basePath = Paths.get(basePathString).normalize();
 
-  if (Files.isRegularFile(path)) {
-    if (checkIgnoredFiles(path.toString())) {
-      filesToZip.add(path);
+    // Validate base path
+    if (!Files.exists(basePath) || !Files.isDirectory(basePath)) {
+      throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_BASEPATHINVALID"), basePath.toString()));
     }
-  } else if (Files.isDirectory(path)) {
-    // Add all files in directory recursively
-    Files.walkFileTree(path, getSimpleFileVisitor(filesToZip));
-  } else {
-    throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_INVALIDPATH"), path.toString()));
-  }
-}
 
-  /**
- * Handles paths with wildcards and adds matching files to the provided set.
- * This method processes the given search path, identifies the base path and glob pattern,
- * and walks the file tree from the base path to find matching files.
- *
- * @param searchPath The search path containing wildcards.
- * @param filesToZip The set to which matching files will be added.
- * @throws IOException If an I/O error occurs during file tree traversal.
- */
-private static void handleWildcardPath(String searchPath, Set<Path> filesToZip) throws IOException {
-  String globPattern;
-  Path basePath;
-  // Handle paths with wildcards
-  int firstWildcardIndex = indexOfWildcard(searchPath);
-  int lastSeparatorBeforeWildcard = searchPath.lastIndexOf(File.separator, firstWildcardIndex);
+    // Glob pattern is the rest of the path after the base path
+    String patternAfterBasePath = StringUtils.substring(searchPath, lastSeparatorBeforeWildcard + 1);
+    globPattern = "glob:" + patternAfterBasePath;
 
-  // Base path is up to the last separator before the first wildcard
-  String basePathString = (lastSeparatorBeforeWildcard >= 0)
-      ? searchPath.substring(0, lastSeparatorBeforeWildcard)
-      : ".";
-  basePath = Paths.get(basePathString).normalize();
+    // Create PathMatcher
+    PathMatcher matcher = FileSystems.getDefault().getPathMatcher(globPattern);
 
-  // Validate base path
-  if (!Files.exists(basePath) || !Files.isDirectory(basePath)) {
-    throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_BASEPATHINVALID"), basePath.toString()));
+    // Walk file tree from base path
+    Files.walkFileTree(basePath, getSimpleFileVisitor(basePath, matcher, filesToZip));
   }
 
-  // Glob pattern is the rest of the path after the base path
-  String patternAfterBasePath = searchPath.substring(lastSeparatorBeforeWildcard + 1);
-  globPattern = "glob:" + patternAfterBasePath;
-
-  // Create PathMatcher
-  PathMatcher matcher = FileSystems.getDefault().getPathMatcher(globPattern);
-
-  // Walk file tree from base path
-  Files.walkFileTree(basePath, getSimpleFileVisitor(basePath, matcher, filesToZip));
-}
-
   /**
- * Creates a SimpleFileVisitor to visit files and add matching files to the provided set.
- * This method returns a SimpleFileVisitor that checks each visited file against the provided PathMatcher
- * and adds it to the set if it matches and is not ignored.
- *
- * @param basePath The base path to relativize the file paths.
- * @param matcher The PathMatcher to check if the file matches the pattern.
- * @param filesToZip The set to which matching files will be added.
- * @return A SimpleFileVisitor that processes files as described.
- */
-private static SimpleFileVisitor<Path> getSimpleFileVisitor(Path basePath, PathMatcher matcher,
-    Set<Path> filesToZip) {
-  return new SimpleFileVisitor<Path>() {
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-      Path relativePath = basePath.relativize(file);
-      if (matcher.matches(relativePath) && checkIgnoredFiles(file.toString())) {
-        filesToZip.add(file);
+   * Creates a SimpleFileVisitor to visit files and add matching files to the provided set.
+   * This method returns a SimpleFileVisitor that checks each visited file against the provided PathMatcher
+   * and adds it to the set if it matches and is not ignored.
+   *
+   * @param basePath
+   *     The base path to relativize the file paths.
+   * @param matcher
+   *     The PathMatcher to check if the file matches the pattern.
+   * @param filesToZip
+   *     The set to which matching files will be added.
+   * @return A SimpleFileVisitor that processes files as described.
+   */
+  private static SimpleFileVisitor<Path> getSimpleFileVisitor(Path basePath, PathMatcher matcher,
+      Set<Path> filesToZip) {
+    return new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        Path relativePath = basePath.relativize(file);
+        if (matcher.matches(relativePath) && checkIgnoredFiles(file.toString())) {
+          filesToZip.add(file);
+        }
+        return FileVisitResult.CONTINUE;
       }
-      return FileVisitResult.CONTINUE;
-    }
-  };
-}
-
-/**
- * Creates a SimpleFileVisitor to visit files and add non-ignored files to the provided set.
- * This method returns a SimpleFileVisitor that adds each visited file to the set if it is not ignored.
- *
- * @param filesToZip The set to which non-ignored files will be added.
- * @return A SimpleFileVisitor that processes files as described.
- */
-private static SimpleFileVisitor<Path> getSimpleFileVisitor(Set<Path> filesToZip) {
-  return new SimpleFileVisitor<Path>() {
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-      if (checkIgnoredFiles(file.toString())) {
-        filesToZip.add(file);
-      }
-      return FileVisitResult.CONTINUE;
-    }
-  };
-}
-
-  /**
- * Finds the index of the first wildcard character ('*' or '?') in the given path string.
- * This method searches for the first occurrence of '*' and '?' in the provided path string
- * and returns the index of the first one found. If neither is found, it returns -1.
- *
- * @param path The path string to search for wildcard characters.
- * @return The index of the first wildcard character, or -1 if none are found.
- */
-private static int indexOfWildcard(String path) {
-  int indexAsterisk = path.indexOf('*');
-  int indexQuestion = path.indexOf('?');
-
-  if (indexAsterisk == -1) return indexQuestion;
-  if (indexQuestion == -1) return indexAsterisk;
-
-  return Math.min(indexAsterisk, indexQuestion);
-}
-
-
-  /**
- * Creates a ZIP file containing the specified set of files.
- * This method generates a temporary ZIP file and adds each file from the provided set to the ZIP file.
- *
- * @param filesToZip The set of file paths to be included in the ZIP file.
- * @return The created ZIP file.
- * @throws IOException If an I/O error occurs during the creation of the ZIP file.
- */
-private static File getZipFile(Set<Path> filesToZip) throws IOException {
-  // Create the ZIP file
-  File zipFile = File.createTempFile("files", ".zip");
-  try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile))) {
-    int i = 0;
-    int total = filesToZip.size();
-    for (Path filePath : filesToZip) {
-      ZipEntry zipEntry = new ZipEntry(filePath.toString());
-      zos.putNextEntry(zipEntry);
-      Files.copy(filePath, zos);
-      zos.closeEntry();
-      logIfDebug(log, String.format("Added file %s to zip file. %d of %d%n", filePath.toString(), ++i, total));
-    }
+    };
   }
-  return zipFile;
-}
+
+  /**
+   * Creates a SimpleFileVisitor to visit files and add non-ignored files to the provided set.
+   * This method returns a SimpleFileVisitor that adds each visited file to the set if it is not ignored.
+   *
+   * @param filesToZip
+   *     The set to which non-ignored files will be added.
+   * @return A SimpleFileVisitor that processes files as described.
+   */
+  private static SimpleFileVisitor<Path> getSimpleFileVisitor(Set<Path> filesToZip) {
+    return new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (checkIgnoredFiles(file.toString())) {
+          filesToZip.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+      }
+    };
+  }
+
+  /**
+   * Finds the index of the first wildcard character ('*' or '?') in the given path string.
+   * This method searches for the first occurrence of '*' and '?' in the provided path string
+   * and returns the index of the first one found. If neither is found, it returns -1.
+   *
+   * @param path
+   *     The path string to search for wildcard characters.
+   * @return The index of the first wildcard character, or -1 if none are found.
+   */
+  private static int indexOfWildcard(String path) {
+    int indexAsterisk = StringUtils.indexOf(path, '*');
+    int indexQuestion = StringUtils.indexOf(path, '?');
+
+    if (indexAsterisk == -1) return indexQuestion;
+    if (indexQuestion == -1) return indexAsterisk;
+
+    return Math.min(indexAsterisk, indexQuestion);
+  }
+
+
+  /**
+   * Creates a ZIP file containing the specified set of files.
+   * This method generates a temporary ZIP file and adds each file from the provided set to the ZIP file.
+   *
+   * @param filesToZip
+   *     The set of file paths to be included in the ZIP file.
+   * @return The created ZIP file.
+   * @throws IOException
+   *     If an I/O error occurs during the creation of the ZIP file.
+   */
+  private static File getZipFile(Set<Path> filesToZip) throws IOException {
+    // Create the ZIP file
+    File zipFile = File.createTempFile("files", ".zip");
+    try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile))) {
+      int i = 0;
+      int total = filesToZip.size();
+      for (Path filePath : filesToZip) {
+        ZipEntry zipEntry = new ZipEntry(filePath.toString());
+        zos.putNextEntry(zipEntry);
+        Files.copy(filePath, zos);
+        zos.closeEntry();
+        logIfDebug(log, String.format("Added file %s to zip file. %d of %d%n", filePath.toString(), ++i, total));
+      }
+    }
+    return zipFile;
+  }
 
   /**
    * Checks if the given path string contains any of the ignored substrings.
@@ -251,20 +267,21 @@ private static File getZipFile(Set<Path> filesToZip) throws IOException {
   }
 
   /**
- * Retrieves the attachment associated with the given CopilotFile.
- * This method creates a criteria query to find an attachment that matches the specified CopilotFile.
- *
- * @param targetInstance The CopilotFile instance for which the attachment is to be retrieved.
- * @return The Attachment associated with the given CopilotFile, or null if no attachment is found.
- */
-public static Attachment getAttachment(CopilotFile targetInstance) {
-  OBCriteria<Attachment> attchCriteria = OBDal.getInstance().createCriteria(Attachment.class);
-  attchCriteria.add(Restrictions.eq(Attachment.PROPERTY_RECORD, targetInstance.getId()));
-  attchCriteria.add(Restrictions.eq(Attachment.PROPERTY_TABLE,
-      OBDal.getInstance().get(Table.class, COPILOT_FILE_AD_TABLE_ID)));
-  attchCriteria.add(Restrictions.ne(Attachment.PROPERTY_ID, targetInstance.getId()));
-  return (Attachment) attchCriteria.setMaxResults(1).uniqueResult();
-}
+   * Retrieves the attachment associated with the given CopilotFile.
+   * This method creates a criteria query to find an attachment that matches the specified CopilotFile.
+   *
+   * @param targetInstance
+   *     The CopilotFile instance for which the attachment is to be retrieved.
+   * @return The Attachment associated with the given CopilotFile, or null if no attachment is found.
+   */
+  public static Attachment getAttachment(CopilotFile targetInstance) {
+    OBCriteria<Attachment> attchCriteria = OBDal.getInstance().createCriteria(Attachment.class);
+    attchCriteria.add(Restrictions.eq(Attachment.PROPERTY_RECORD, targetInstance.getId()));
+    attchCriteria.add(Restrictions.eq(Attachment.PROPERTY_TABLE,
+        OBDal.getInstance().get(Table.class, COPILOT_FILE_AD_TABLE_ID)));
+    attchCriteria.add(Restrictions.ne(Attachment.PROPERTY_ID, targetInstance.getId()));
+    return (Attachment) attchCriteria.setMaxResults(1).uniqueResult();
+  }
 
   /**
    * Executes the hook for a given CopilotFile.
@@ -307,18 +324,20 @@ public static Attachment getAttachment(CopilotFile targetInstance) {
   }
 
   /**
- * Removes the attachment associated with the given CopilotFile.
- * This method retrieves the attachment for the specified CopilotFile and deletes it using the provided AttachImplementationManager.
- *
- * @param aim The AttachImplementationManager instance used to delete the attachment.
- * @param hookObject The CopilotFile instance for which the attachment is to be removed.
- */
-private void removeAttachment(AttachImplementationManager aim, CopilotFile hookObject) {
-  Attachment attachment = getAttachment(hookObject);
-  if (attachment != null) {
-    aim.delete(attachment);
+   * Removes the attachment associated with the given CopilotFile.
+   * This method retrieves the attachment for the specified CopilotFile and deletes it using the provided AttachImplementationManager.
+   *
+   * @param aim
+   *     The AttachImplementationManager instance used to delete the attachment.
+   * @param hookObject
+   *     The CopilotFile instance for which the attachment is to be removed.
+   */
+  private void removeAttachment(AttachImplementationManager aim, CopilotFile hookObject) {
+    Attachment attachment = getAttachment(hookObject);
+    if (attachment != null) {
+      aim.delete(attachment);
+    }
   }
-}
 
   /**
    * Checks if the hook is applicable for the given type.
