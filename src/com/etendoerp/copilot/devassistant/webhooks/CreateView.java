a9608d5eeedab1c1a31ed1a5e821ec7c25dae116
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2025-01-09 13:47:56 -0300

    Feature ETP-844: Add Validations in Create View

--- src/com/etendoerp/copilot/devassistant/webhooks/CreateView.java
+++ src/com/etendoerp/copilot/devassistant/webhooks/CreateView.java
@@ -4,14 +4,18 @@ import static com.etendoerp.copilot.util.OpenAIUtils.logIfDebug;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 
@@ -40,10 +44,17 @@ public class CreateView extends BaseWebhookService {
         name = StringUtils.removeStart(name, prefix).substring(1);
       }
 
+      String viewDbName = String.format("%s_%s", prefix, name);
+      if (!StringUtils.endsWithIgnoreCase(viewDbName, "_v")) {
+        viewDbName = viewDbName + "_v";
+      }
+
+      checkProyections(querySelect, viewDbName);
+
       String query = String.format(
-          "CREATE OR REPLACE VIEW public.%s_%s_v" +
-          " AS %s",
-          prefix, name, querySelect
+          "CREATE OR REPLACE VIEW public." + viewDbName +
+              " AS %s",
+          querySelect
       );
 
       PreparedStatement statement = conn.prepareStatement(query);
@@ -54,6 +65,49 @@ public class CreateView extends BaseWebhookService {
 
     } catch (Exception e) {
       responseVars.put("error", e.getMessage());
+    }
+  }
+
+  private void checkProyections(String querySelect, String viewDbName) {
+    //execute the query  with a limit 0 to check if the query is valid
+    //if the query ends with ; remove it
+    if (querySelect.endsWith(";")) {
+      querySelect = querySelect.substring(0, querySelect.length() - 1);
+    }
+    String query = String.format("SELECT * FROM (%s) AS %s LIMIT 0", querySelect, viewDbName);
+    Connection conn = OBDal.getInstance().getConnection();
+    try {
+      PreparedStatement statement = conn.prepareStatement(query);
+      statement.execute();
+      ResultSet resultSet = statement.getResultSet();
+
+      //get the columnnames from the resultset
+      List<String> columnList = new ArrayList<>();
+      int columnCount = resultSet.getMetaData().getColumnCount();
+      for (int i = 1; i <= columnCount; i++) {
+        String columnName = resultSet.getMetaData().getColumnName(i);
+        LOG.info("Column Name: " + columnName);
+        columnList.add(columnName);
+      }
+      LOG.info("Column List: " + Arrays.toString(columnList.toArray()));
+      //check that exists the column _id for the view and the mandatory columns
+      if (columnList.stream().noneMatch(col -> StringUtils.equalsIgnoreCase(col, viewDbName + "_id"))) {
+        throw new OBException(
+            String.format(OBMessageUtils.messageBD("COPDEV_ProjectionColumnNotFound"), viewDbName, viewDbName + "_id"));
+      }
+      List<String> mandatoryColums = Arrays.asList("ad_client_id", "ad_org_id", "isactive", "created", "createdby",
+          "updated", "updatedby");
+      List<String> missingCols = mandatoryColums.stream().filter(
+          missingCol -> columnList.stream().noneMatch(col -> StringUtils.equalsIgnoreCase(col, missingCol))).collect(
+          Collectors.toList());
+      if (!missingCols.isEmpty()) {
+        throw new OBException(String.format(OBMessageUtils.messageBD("COPDEV_ProjectionColumnNotFound"), viewDbName,
+            missingCols.toString()));
+      }
+
+
+    } catch (Exception e) {
+      throw new OBException(OBMessageUtils.messageBD("COPDEV_InvalidQuery"));
     }
   }
 
