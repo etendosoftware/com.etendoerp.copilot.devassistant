40006d6eeef852c8e579bebea9e4b24d08509637
Matias Bernal <matias.bernal@etendo.software>
2025-04-22 17:31:15 -0300

    Feature ETP-1534: Update module to support Etendo 25Q1

--- src/com/etendoerp/copilot/devassistant/webhooks/ElementsHandler.java
+++ src/com/etendoerp/copilot/devassistant/webhooks/ElementsHandler.java
@@ -6,7 +6,7 @@ import static com.etendoerp.copilot.devassistant.Utils.logIfDebug;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
--- src/com/etendoerp/copilot/devassistant/webhooks/RegisterFields.java
+++ src/com/etendoerp/copilot/devassistant/webhooks/RegisterFields.java
@@ -6,7 +6,7 @@ import static com.etendoerp.copilot.devassistant.Utils.logExecutionInit;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openbravo.dal.service.OBDal;
