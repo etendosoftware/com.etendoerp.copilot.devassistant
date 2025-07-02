2af1c4cd43a29f656afd6dbcb365017432e2e6d9
Francisco Roig <francisco.roig@smfconsulting.es>
2025-07-02 19:39:56 -0300

    Feature ETP-1793: Add missing import to webhook test

--- src-test/src/com/etendoerp/copilot/devassistant/webhooks/DevAssistantWebhooksTests.java
+++ src-test/src/com/etendoerp/copilot/devassistant/webhooks/DevAssistantWebhooksTests.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
