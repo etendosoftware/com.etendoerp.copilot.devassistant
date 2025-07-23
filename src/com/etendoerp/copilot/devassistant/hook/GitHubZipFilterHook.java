d47baa4cf7652bfd63344d54fab42eff6f44a121
sebastianbarrozo <sebastian.barrozo@etendo.software>
2025-07-23 15:44:36 -0300

    Feature ETP-2048: Migrate lang to lang3 imports

--- src/com/etendoerp/copilot/devassistant/hook/GitHubZipFilterHook.java
+++ src/com/etendoerp/copilot/devassistant/hook/GitHubZipFilterHook.java
@@ -27,7 +27,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
